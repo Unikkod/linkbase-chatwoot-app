2815a0a942c48660bd6dbe20a467e5cfe0dbae4d
Victor K <victorkrusenstrahle@gmail.com>
2023-10-13 18:41:07 +0200

    Added columns component

--- app/page.tsx
+++ app/page.tsx
@@ -5,26 +5,28 @@ import { useContext } from 'react';
 import { ChatwootContext } from '@/context/ChatwootContext';
 
 import CardWithHeader from '@/components/CardWithHeader/CardWithHeader';
-import Container from '@/components/Container/Container';
 import LabelAndValue from '@/components/LabelAndValue/LabelAndValue';
+import Columns from '@/components/Columns/Columns';
 
 export default function Home() {
   const { contact } = useContext(ChatwootContext);
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between py-24'>
-      <Container>
-        {contact ? (
+    <main className='flex min-h-screen flex-col items-center justify-between py-10'>
+      {contact ? (
+        <Columns leftColumn={<CardWithHeader header='Customer'>Left</CardWithHeader>} rightColumn={<CardWithHeader header='Customer'>Right</CardWithHeader>}>
           <CardWithHeader header='Customer'>
             <div className='flex flex-col gap-4'>
               <LabelAndValue label='Name' value={contact.name} />
               <LabelAndValue label='Email' value={contact.email} />
               <LabelAndValue label='Supabase User ID' value={contact.custom_attributes?.supabase_user_id} />
+              <LabelAndValue label='Organisation' value={contact.custom_attributes?.organisation} />
+              <LabelAndValue label='Company' value={contact.custom_attributes?.company} />
             </div>
           </CardWithHeader>
-        ) : (
-          <p>No contact found</p>
-        )}
-      </Container>
+        </Columns>
+      ) : (
+        <p>No contact found</p>
+      )}
     </main>
   );
 }
---
+++ components/Columns/Columns.tsx
@@ -0,0 +1,19 @@
+interface ColumnsProps {
+  leftColumn: React.ReactNode;
+  children: React.ReactNode;
+  rightColumn: React.ReactNode;
+}
+
+const Columns = ({ leftColumn, children, rightColumn }: ColumnsProps) => {
+  return (
+    <div className='mx-auto w-full max-w-7xl grow lg:flex xl:px-2'>
+      <div className='flex-1 xl:flex'>
+        <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:w-64 xl:shrink-0 xl:pl-6'>{leftColumn}</div>
+        <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>{children}</div>
+      </div>
+      <div className='shrink-0 px-4 py-6 sm:px-6 lg:w-96 lg:pr-8 xl:pr-6'>{rightColumn}</div>
+    </div>
+  );
+};
+
+export default Columns;
