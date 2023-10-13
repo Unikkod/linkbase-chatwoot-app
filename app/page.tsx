368ed92785eb95eb54fe1e9a1534c58200be65f1
Victor K <victorkrusenstrahle@gmail.com>
2023-10-13 14:38:09 +0200

    Moved logic into hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,18 @@
-"use client";
-
-import { useEffect } from 'react';
+'use client';
 
 import CardWithHeader from '@/components/CardWithHeader/CardWithHeader';
 import Container from '@/components/Container/Container';
 
+import useChatwoot from '@/hooks/useChatwoot';
+
 export default function Home() {
+  const { contact, conversation, agent } = useChatwoot();
 
-
-
-	useEffect(() => {
-		const handleResponse = (event: any) => {
-			if (typeof event.data !== 'string' || !isJSONValid(event.data)) {
-				return;
-			}
-			const { data } = JSON.parse(event.data);
-
-      const { contact, conversation, currentAgent: agent } = data;
-
-      console.log('Contact', contact);
-      console.log('Conversation', conversation);
-      console.log('Agent', agent);
-		}
-
-		window.addEventListener('message', handleResponse);
-
-		// Request data from Chatwoot
-		window.parent.postMessage('chatwoot-dashboard-app:fetch-info', '*');
-
-		// Clean up the event listener when the component unmounts
-		return () => {
-			window.removeEventListener('message', handleResponse);
-		};
-	}, []);
-
-  const isJSONValid = (data: string) => {
-		try {
-			JSON.parse(data);
-		} catch (error: any) {
-			return false;
-		}
-		return true;
-	}
-
-	return (
-		<main className='flex min-h-screen flex-col items-center justify-between py-24'>
-			<Container>
-				<CardWithHeader header='Hello World'>Test</CardWithHeader>
-			</Container>
-		</main>
-	);
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between py-24'>
+      <Container>
+        <CardWithHeader header='Hello World'>{contact.name} {contact.email} {contact.custom_attributes?.supabase_user_id}</CardWithHeader>
+      </Container>
+    </main>
+  );
 }
---
+++ hooks/useChatwoot.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+
+import { contact as initialState } from '@/modules/contact';
+import { Contact } from '@/types/ContactTypes';
+
+const useChatwoot = () => {
+  const [contact, setContact] = useState<Contact | null>(null);
+  const [conversation, setConversation] = useState<any | null>(null);
+  const [agent, setAgent] = useState<any | null>(null);
+
+  const isJSONValid = (data: string) => {
+    try {
+      JSON.parse(data);
+    } catch (error: any) {
+      return false;
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    const handleResponse = (event: any) => {
+      if (typeof event.data !== 'string' || !isJSONValid(event.data)) {
+        return;
+      }
+      const { data } = JSON.parse(event.data);
+
+      const { contact, conversation, currentAgent: agent } = data;
+
+      setContact(contact);
+      setConversation(conversation);
+      setAgent(agent);
+    };
+
+    window.addEventListener('message', handleResponse);
+
+    // Request data from Chatwoot
+    window.parent.postMessage('chatwoot-dashboard-app:fetch-info', '*');
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('message', handleResponse);
+    };
+  }, []);
+
+  return { contact, conversation, agent };
+};
+
+export default useChatwoot;
