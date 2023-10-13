1cbb78eab9009967ef6d7aea2716e285652bcb9e
Victor K <victorkrusenstrahle@gmail.com>
2023-10-13 13:29:24 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,44 @@
+"use client";
+
+import { useEffect } from 'react';
+
 import CardWithHeader from '@/components/CardWithHeader/CardWithHeader';
 import Container from '@/components/Container/Container';
 
 export default function Home() {
-	if (typeof window !== 'undefined') {
-		const response = window.parent.postMessage('chatwoot-dashboard-app:fetch-info', '*');
-		console.log(response);
+	const isJSONValid = (data: string) => {
+		try {
+			JSON.parse(data);
+		} catch (e) {
+			return false;
+		}
+		return true;
 	}
+
+	useEffect(() => {
+		const handleResponse = (event: any) => {
+      console.log('Received event from Chatwoot');
+
+			if (typeof event.data !== 'string' || !isJSONValid(event.data)) {
+				return;
+			}
+
+			const eventData = JSON.parse(event.data);
+			if (eventData.type === 'chatwoot-dashboard-app:fetch-info') {
+				console.log(eventData.payload);
+			}
+		}
+
+		window.addEventListener('message', handleResponse);
+
+		// Request data from Chatwoot
+		window.parent.postMessage('chatwoot-dashboard-app:fetch-info', '*');
+
+		// Clean up the event listener when the component unmounts
+		return () => {
+			window.removeEventListener('message', handleResponse);
+		};
+	}, []);
 
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between py-24'>
