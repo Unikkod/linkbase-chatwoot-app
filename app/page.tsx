fc905b9e05344b90be41d38fb62c1a3d75ae2018
Victor K <victorkrusenstrahle@gmail.com>
2023-10-13 13:47:22 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,44 +6,42 @@ import CardWithHeader from '@/components/CardWithHeader/CardWithHeader';
 import Container from '@/components/Container/Container';
 
 export default function Home() {
-	const isJSONValid = (data: string) => {
-		try {
-			JSON.parse(data);
-		} catch (error: any) {
-      console.log('Invalid JSON', error);
-			return false;
-		}
-		return true;
-	}
+
+
 
 	useEffect(() => {
 		const handleResponse = (event: any) => {
-      console.log('Received event from Chatwoot', event);
-
 			if (typeof event.data !== 'string' || !isJSONValid(event.data)) {
 				return;
 			}
+			const { data } = JSON.parse(event.data);
 
-      console.log('Is valid JSON');
+      const { contact, conversation, currentAgent: agent } = data;
 
-			const eventData = JSON.parse(event.data);
-      console.log('eventData', eventData);
-			if (eventData.type === 'chatwoot-dashboard-app:fetch-info') {
-        console.log('eventData.payload', eventData.payload);
-			}
+      console.log('Contact', contact);
+      console.log('Conversation', conversation);
+      console.log('Agent', agent);
 		}
 
 		window.addEventListener('message', handleResponse);
 
 		// Request data from Chatwoot
-		const response = window.parent.postMessage('chatwoot-dashboard-app:fetch-info', '*');
-		console.log('response', response);
+		window.parent.postMessage('chatwoot-dashboard-app:fetch-info', '*');
 
 		// Clean up the event listener when the component unmounts
 		return () => {
 			window.removeEventListener('message', handleResponse);
 		};
 	}, []);
+
+  const isJSONValid = (data: string) => {
+		try {
+			JSON.parse(data);
+		} catch (error: any) {
+			return false;
+		}
+		return true;
+	}
 
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between py-24'>
