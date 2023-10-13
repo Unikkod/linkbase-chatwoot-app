0fb991dac61f67668bf4c3f81eff2f8a31a72422
Victor K <victorkrusenstrahle@gmail.com>
2023-10-13 20:53:32 +0200

    Update LabelAndValue.tsx

--- components/LabelAndValue/LabelAndValue.tsx
+++ components/LabelAndValue/LabelAndValue.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const LabelAndValue = ({ label, value }: { label: string; value: string | number | undefined }) => (
   <React.Fragment>
-    {value === undefined ? (
+    {value === undefined ? null : (
       <div className='flex flex-col gap-2'>
         <p className='font-medium text-gray-800'>{label}</p>
         <p>{value}</p>
       </div>
-    ) : null}
+    )}
   </React.Fragment>
 );
 
